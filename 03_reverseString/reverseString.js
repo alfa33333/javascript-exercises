c871047a429e78d1e054a482f2978af06c2b47dd
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2023-12-11 19:32:10 -0500

    complete the reverse array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let array = string.split('');
+    let reversedString = array.reverse().join('');
+
+    return reversedString;
 };
 
 // Do not edit below this line
