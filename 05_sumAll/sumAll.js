2f8f106e7d56ce8f8c211025300ec631c0e502e2
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2023-12-31 17:51:37 -0600

    Refactor sumAll function to handle negative numbers and non-number parameters

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    let sum = 0;
+    let begin = 0;
+    let end = 0;
+    if (first > second) {  
+        begin = second;
+        end = first;
+    }
+    else {
+        begin = first;
+        end = second;
+    }
 
+    if (begin < 0 || end < 0 || begin !==Number(begin) || end !== Number(end)) {
+        return 'ERROR';
+    }
+
+    for (let i = begin; i <= end; i++) {
+        sum += i;
+    }
+     return sum;
 };
 
 // Do not edit below this line
