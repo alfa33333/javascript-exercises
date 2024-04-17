22cdf3ada707df8b1c30f107534cedfc6417f491
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2024-04-17 19:08:30 -0400

    Refactor palindromes and fibonacci functions

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let inputString = input.toLowerCase().replace(/[^A-Za-z0-9]/g, '')
+    let reversedString = inputString.split('').reverse().join('');
+    return inputString === reversedString;
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(positionInput) {
+    fib = [0, 1];
+    position = parseInt(positionInput, 10);
+    if (position < 0)
+    {
+        return "OOPS";
+    } else if (position >= 2)
+    {
+        for (let i = 2; i <= position; i++)
+        {
+            temp = fib.reduce((a, b) => a + b, 0);
+            fib[0] = fib[1];
+            fib[1] = temp;
+        }
+    }
+    return fib[1];
 };
 
 // Do not edit below this line
