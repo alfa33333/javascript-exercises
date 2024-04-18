cfbd4d5531d1d34dcdd610125e328e979d5cc534
alfa33333 <44150484+alfa33333@users.noreply.github.com>
2024-04-17 22:10:44 -0400

    Refactor getTheTitles function to accept an array of books in getTheTitles.js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+     return people.reduce((oldestPerson, currentPerson) => { 
+        let currentYear = new Date().getFullYear();
+        if (!oldestPerson.hasOwnProperty('yearOfDeath')) {
+            oldestPerson.yearOfDeath = currentYear;
+        }
+        if (!currentPerson.hasOwnProperty('yearOfDeath')) {
+            currentPerson.yearOfDeath = currentYear;
+        }
+        let oldestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+        let currentAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
 
+        return oldestAge > currentAge ? oldestPerson : currentPerson;});
+    // return people.sort((a, b) => { return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth); })[people.length - 1] ;
 };
 
 // Do not edit below this line
